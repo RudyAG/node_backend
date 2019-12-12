830ec77a86992b6c52a4f10c553b7d07a6768aad
rudy <rudy.agot@edu.ece.fr>
2019-12-12 13:18:48 +0100

    reinit 3-storage

--- 3-storage/leveldb.ts
+++
@@ -1,8 +0,0 @@
-import encoding from 'encoding-down'
-import leveldown from 'leveldown'
-import levelup from 'levelup'
-
-const db = levelup(encoding(
-  leveldown("path"), 
-  { valueEncoding: 'json' })
-)
